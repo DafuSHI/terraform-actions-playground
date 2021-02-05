dffdbe56fbb9cdf2f1b8b7985c73bec2d9a014d0
DafuSHI <shidafu920704@gmail.com>
2021-02-05 14:55:03 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,7 +29,20 @@ variable "nat_gateway_name" {
 
 //eip
 
+terraform {
+  required_providers {
+    flexibleengine = {
+      source = "FlexibleEngineCloud/flexibleengine"
+      version = "1.16.2"
+    }
+  }
+  required_version = ">= 0.13"
+}
 
+provider "flexibleengine" {
+  auth_url    = "https://iam.eu-west-0.prod-cloud-ocb.orange-business.com/v3"
+  region      = "eu-west-0"
+}
 
 resource "flexibleengine_vpc_v1" "vpc_v1" {
   name = var.vpc_name
