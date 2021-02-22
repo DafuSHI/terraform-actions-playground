7c1b629e1eabab40bfe07c5f9ddbc25f9c8c5e1f
DafuSHI <shidafu920704@gmail.com>
2021-02-22 16:33:17 +0100

    Test CCE Node Pool

--- main.tf
+++ main.tf
@@ -61,14 +61,6 @@ resource "flexibleengine_vpc_subnet_v1" "subnet_v1" {
   vpc_id     = flexibleengine_vpc_v1.vpc_v1.id
 }
 
-resource "flexibleengine_nat_gateway_v2" "nat_1" {
-  name                = var.nat_gateway_name
-  description         = "lo connector only needs access to liveobject endpoint, no need to expose to outside with public ip"
-  spec                = "2"
-  router_id           = flexibleengine_vpc_v1.vpc_v1.id
-  internal_network_id = flexibleengine_vpc_subnet_v1.subnet_v1.id
-}
-
 resource "flexibleengine_vpc_eip_v1" "eip_1" {
   publicip {
     type = "5_bgp"
@@ -81,13 +73,49 @@ resource "flexibleengine_vpc_eip_v1" "eip_1" {
   }
 }
 
-resource "flexibleengine_nat_snat_rule_v2" "snat_1" {
-  nat_gateway_id = flexibleengine_nat_gateway_v2.nat_1.id
-  network_id     = flexibleengine_vpc_subnet_v1.subnet_v1.id
-  floating_ip_id = flexibleengine_vpc_eip_v1.eip_1.id
-}
-
 
 output "flavors" {
   value = data.flexibleengine_compute_availability_zones_v2.myaz.names[0]
 }
+
+resource "flexibleengine_compute_keypair_v2" "keypair" {
+  name       = "my-keypair"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDAjpC1hwiOCCmKEWxJ4qzTTsJbKzndLotBCz5PcwtUnflmU+gHJtWMZKpuEGVi29h0A/+ydKek1O18k10Ff+4tyFjiHDQAnOfgWf7+b1yK+qDip3X1C0UPMbwHlTfSGWLGZqd9LvEFx9k3h/M+VtMvwR1lJ9LUyTAImnNjWG7TaIPmui30HvM2UiFEmqkr4ijq45MyX2+fLIePLRIF61p4whjHAQYufqyno3BS48icQb4p6iVEZPo4AE2o9oIyQvj2mx4dk5Y8CgSETOZTYDOR3rU2fZTRDRgPJDH9FWvQjF5tA0p3d9CoWWd2s6GKKbfoUIi8R/Db1BSPJwkqB"
+}
+
+resource "flexibleengine_cce_cluster_v3" "cluster_1" {
+     name = "toto"
+     cluster_type= "VirtualMachine"
+     flavor_id= "cce.s2.medium"
+     vpc_id= flexibleengine_vpc_v1.vpc_v1.id
+     subnet_id= flexibleengine_vpc_subnet_v1.subnet_v1.id
+     container_network_type= "overlay_l2"
+     authentication_mode = "rbac"
+     description= "Create cluster"
+    }
+
+resource "flexibleengine_cce_node_pool_v3" "node_pool" {
+  cluster_id               = flexibleengine_cce_cluster_v3.cluster_1.id
+  name                     = "testpool"
+  os                       = "CentOS 7.7"
+  initial_node_count       = 2
+  flavor_id                = "s3.large.4"
+  availability_zone        = null
+  key_pair                 = flexibleengine_compute_keypair_v2.keypair.id
+  scall_enable             = true
+  min_node_count           = 1
+  max_node_count           = 10
+  scale_down_cooldown_time = 100
+  priority                 = 1
+  type                     = "vm"
+
+  root_volume {
+    size       = 40
+    volumetype = "SAS"
+  }
+  data_volumes {
+    size       = 100
+    volumetype = "SAS"
+  }
+}
+
