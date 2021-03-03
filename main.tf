6857d79ce3d03675d6690917ec446d494dea1c88
DafuSHI <shidafu920704@gmail.com>
2021-03-03 13:47:46 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -73,49 +73,25 @@ resource "flexibleengine_vpc_eip_v1" "eip_1" {
   }
 }
 
-
-output "flavors" {
-  value = data.flexibleengine_compute_availability_zones_v2.myaz.names[0]
+resource "flexibleengine_vpc_eip_v1" "rest_api_eip" {
+  publicip {
+    type = "5_bgp"
+  }
+  bandwidth {
+    name = "rest_api_eip_bandwidth"
+    size = "1"
+    share_type = "PER"
+    charge_mode = "traffic"
+  }
 }
 
+#ENHANCED LOAD BALANCER
+resource "flexibleengine_lb_loadbalancer_v2" "elb_rest_api" {
+  name              = "elb_rest_api"
+  admin_state_up    = true
+  vip_subnet_id     = flexibleengine_vpc_subnet_v1.subnet_v1.id
+  vip_address       = "${flexibleengine_vpc_eip_v1.rest_api_eip.publicip[0].ip_address}"
+  depends_on        = ["flexibleengine_vpc_eip_v1.rest_api_eip"]
 
-resource "flexibleengine_cce_cluster_v3" "cluster_1" {
-     name = "toto"
-     cluster_type= "VirtualMachine"
-     flavor_id= "cce.s2.medium"
-     vpc_id= flexibleengine_vpc_v1.vpc_v1.id
-     subnet_id= flexibleengine_vpc_subnet_v1.subnet_v1.id
-     container_network_type= "overlay_l2"
-     authentication_mode = "rbac"
-     description= "Create cluster"
-    }
-
-resource "flexibleengine_cce_node_pool_v3" "node_pool" {
-  cluster_id               = flexibleengine_cce_cluster_v3.cluster_1.id
-  name                     = "testpool"
-  os                       = "CentOS 7.7"
-  initial_node_count       = 2
-  flavor_id                = "s3.large.4"
-  availability_zone        = null
-  key_pair                 = "KeyPair-Dafu-Orange"
-  scall_enable             = true
-  min_node_count           = 1
-  max_node_count           = 10
-  scale_down_cooldown_time = 100
-  priority                 = 1
-  type                     = "vm"
-
-  root_volume {
-    size       = 40
-    volumetype = "SAS"
-  }
-  data_volumes {
-    size       = 100
-    volumetype = "SAS"
-  }
-  timeouts {
-    create = "30m"
-    delete = "2h"
-  }
 }
 
